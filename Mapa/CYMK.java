b5bc1bd85ac824a6534934aef0bd30e81404d4f9
Rodrigo Silva <51206875+Rod634@users.noreply.github.com>
2020-03-04 15:15:42 -0300

    1 questão

---
+++ Mapa/CYMK.java
@@ -0,0 +1,50 @@
+public class CYMK extends Mapas
+{
+    int c;
+    int m;
+    int y;
+    int k;
+ 
+    public CYMK(int c, int m, int y, int k){
+        this.c = c;
+        this.m = m;
+        this.y = y;
+        this.k = k;
+    }
+    
+    public void setC(int c){
+        if(c > 0 && c < 100) this.c = c;
+    }
+    
+    public void setM(int m){
+        if(m > 0 && m < 100) this.m = m;
+    }
+    
+    public void setY(int y){
+        if(y > 0 && y < 100) this.y = y;
+    }
+    
+    public void setK(int k){
+        if(k > 0 && k < 100) this.k = k;
+    }
+    
+    public int getC(){
+        return this.c;
+    }
+    
+    public int getM(){
+        return this.m;
+    }
+    
+    public int getY(){
+        return this.y;
+    }
+    
+    public int getK(){
+        return this.k;
+    }
+    
+    public String tupla(){
+        return "(" + this.c + ", " + this.m + ", " + this.y + ", " + this.k + ")";
+    }
+}
---
+++ Mapa/RGB.java
@@ -0,0 +1,41 @@
+public class RGB extends Mapas
+{
+    int r;
+    int g;
+    int b;
+ 
+    public RGB(int r, int g, int b){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    
+    public void setR(int r){
+        if(r > 0 && r < 255) this.r = r;
+    }
+    
+    public void setG(int g){
+       if(g > 0 && g < 255) this.g = g;
+    }
+    
+    public void setB(int b){
+        if(b > 0 && b < 255) this.b = b;
+    }
+    
+    public int getR(){
+        return this.r;
+    }
+    
+    public int getG(){
+        return this.g;
+    }
+    
+    public int getB(){
+        return this.b;
+    }
+    
+    public String tupla(){
+        return "(" + this.r + ", " + this.g + ", " + this.b + ")";
+    }
+    
+}
